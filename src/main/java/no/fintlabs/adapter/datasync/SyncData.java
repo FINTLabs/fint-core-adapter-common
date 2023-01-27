6d7f5586de62cbf1e9730c13f2eeec068dd601d5
Hknots <Hknots99@gmail.com>
2023-01-27 13:53:20 +0100

    Added Factory methods

--- src/main/java/no/fintlabs/adapter/datasync/SyncData.java
+++ src/main/java/no/fintlabs/adapter/datasync/SyncData.java
@@ -11,4 +11,12 @@ public class SyncData<T extends FintLinks> {
     private final List<T> resources;
     private final SyncDataMethod method;
 
+    public static <T extends FintLinks> SyncData<T> ofPostData(List<T> resources) {
+        return new SyncData<>(resources, SyncDataMethod.POST);
+    }
+
+    public static <T extends FintLinks> SyncData<T> ofPatchData(List<T> resources) {
+        return new SyncData<>(resources, SyncDataMethod.PATCH);
+    }
+
 }
