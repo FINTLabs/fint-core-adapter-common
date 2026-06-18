83d07cfbafd886375520bbc4962e497498c62763
Hknots <hknots99@gmail.com>
2026-06-18 10:36:13 +0200

    feature: add own ReactiveClientProvider

--- src/main/java/no/fintlabs/adapter/config/OAuthConfiguration.kt
+++ src/main/java/no/fintlabs/adapter/config/OAuthConfiguration.kt
@@ -1,92 +1,88 @@
-package no.fintlabs.adapter.config;
+package no.fintlabs.adapter.config
 
-import io.netty.channel.ChannelOption;
-import lombok.Getter;
-import lombok.Setter;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
-import org.springframework.http.client.reactive.ClientHttpConnector;
-import org.springframework.http.client.reactive.ReactorClientHttpConnector;
-import org.springframework.security.oauth2.client.*;
-import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
-import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
-import org.springframework.web.reactive.function.client.ExchangeStrategies;
-import org.springframework.web.reactive.function.client.WebClient;
-import reactor.core.publisher.Mono;
-import reactor.netty.http.client.HttpClient;
-import reactor.netty.resources.ConnectionProvider;
+import io.netty.channel.ChannelOption
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Primary
+import org.springframework.http.client.reactive.ClientHttpConnector
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
+import org.springframework.security.oauth2.client.AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientManager
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientProviderBuilder
+import org.springframework.security.oauth2.client.ReactiveOAuth2AuthorizedClientService
+import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository
+import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction
+import org.springframework.web.reactive.function.client.ExchangeStrategies
+import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
+import reactor.netty.resources.ConnectionProvider
+import java.time.Duration
 
-import java.time.Duration;
-import java.util.Map;
 
-@Slf4j
-@Getter
-@Setter
 @Configuration
-public class OAuthConfiguration {
-    private final AdapterProperties props;
-
-    public OAuthConfiguration(AdapterProperties props) {
-        this.props = props;
-    }
-
-    @Bean
-    public ReactiveOAuth2AuthorizedClientManager authorizedClientManager(
-            ReactiveClientRegistrationRepository clientRegistrationRepository,
-            ReactiveOAuth2AuthorizedClientService authorizedClientService) {
-
-        ReactiveOAuth2AuthorizedClientProvider authorizedClientProvider = ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
-                .password()
-                .refreshToken()
-                .build();
-
-        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager authorizedClientManager = new AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
-                clientRegistrationRepository,
-                authorizedClientService
-        );
-
-        authorizedClientManager.setAuthorizedClientProvider(authorizedClientProvider);
-        authorizedClientManager.setContextAttributesMapper(oAuth2AuthorizeRequest -> Mono.just(Map.of(
-                OAuth2AuthorizationContext.USERNAME_ATTRIBUTE_NAME, props.getUsername(),
-                OAuth2AuthorizationContext.PASSWORD_ATTRIBUTE_NAME, props.getPassword()
-        )));
-
-        return authorizedClientManager;
-    }
-
-    @Bean
-    public ClientHttpConnector clientHttpConnector() {
-        return new ReactorClientHttpConnector(HttpClient.create(
-                        ConnectionProvider
-                                .builder("laidback")
-                                .maxConnections(25)
-                                .pendingAcquireMaxCount(-1)
-                                .pendingAcquireTimeout(Duration.ofMinutes(15))
-                                .maxLifeTime(Duration.ofMinutes(30))
-                                .maxIdleTime(Duration.ofMinutes(5))
-                                .build())
-                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 900000)
-                .responseTimeout(Duration.ofMinutes(10))
-        );
-    }
+class OAuthConfiguration(
+    private val props: AdapterProperties,
+) {
 
     @Bean
     @Primary
-    public WebClient webClient(WebClient.Builder builder, ReactiveOAuth2AuthorizedClientManager authorizedClientManager, ClientHttpConnector clientHttpConnector) {
-        ExchangeStrategies exchangeStrategies = ExchangeStrategies.builder()
-                .codecs(configurer -> configurer.defaultCodecs().maxInMemorySize(-1))
-                .build();
+    fun webClient(
+        authorizedClientManager: ReactiveOAuth2AuthorizedClientManager,
+        clientHttpConnector: ClientHttpConnector
+    ): WebClient = WebClient.builder()
+        .clientConnector(clientHttpConnector)
+        .exchangeStrategies(createExchangeStrategies())
+        .filter(createExchangeFilterFunction(authorizedClientManager))
+        .baseUrl(props.baseUrl)
+        .build()
 
-        ServerOAuth2AuthorizedClientExchangeFilterFunction oauth2Client = new ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager);
-        oauth2Client.setDefaultClientRegistrationId(props.getRegistrationId());
-
-        return builder
-                .clientConnector(clientHttpConnector)
-                .exchangeStrategies(exchangeStrategies)
-                .filter(oauth2Client)
-                .baseUrl(props.getBaseUrl())
-                .build();
+    @Bean
+    fun clientHttpConnector(): ClientHttpConnector {
+        return ReactorClientHttpConnector(
+            HttpClient.create(
+                ConnectionProvider
+                    .builder("laidback")
+                    .maxConnections(25)
+                    .pendingAcquireMaxCount(-1)
+                    .pendingAcquireTimeout(Duration.ofMinutes(15))
+                    .maxLifeTime(Duration.ofMinutes(30))
+                    .maxIdleTime(Duration.ofMinutes(5))
+                    .build()
+            )
+                .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 900000)
+                .responseTimeout(Duration.ofMinutes(10))
+        )
     }
+
+    private fun createExchangeStrategies() = ExchangeStrategies.builder()
+        .codecs { configurer -> configurer.defaultCodecs().maxInMemorySize(-1) }
+        .build();
+
+    private fun createExchangeFilterFunction(authorizedClientManager: ReactiveOAuth2AuthorizedClientManager) =
+        ServerOAuth2AuthorizedClientExchangeFilterFunction(authorizedClientManager)
+            .also { it.setDefaultClientRegistrationId(props.registrationId) }
+
+    @Bean
+    fun authorizedClientManager(
+        clientRegistrationRepository: ReactiveClientRegistrationRepository,
+        authorizedClientService: ReactiveOAuth2AuthorizedClientService,
+    ): ReactiveOAuth2AuthorizedClientManager =
+        AuthorizedClientServiceReactiveOAuth2AuthorizedClientManager(
+            clientRegistrationRepository,
+            authorizedClientService,
+        ).apply {
+            setAuthorizedClientProvider(
+                ReactiveOAuth2AuthorizedClientProviderBuilder.builder()
+                    .provider(passwordProvider())
+                    .refreshToken()
+                    .build()
+            )
+        }
+
+    private fun passwordProvider() =
+        PasswordReactiveOAuth2AuthorizedClientProvider(
+            webClient = WebClient.builder().build(),
+            username = props.username,
+            password = props.password,
+        )
 }
